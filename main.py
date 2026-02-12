2787f16d696f86e0b153eac5c482bc8641602aa8
munenori saito <moo.0822.ktf.g@gmail.com>
2026-02-12 05:42:49 +0000

    change to blue

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import sys
 import numpy as np
 
+
 def main():
     # 画像を読み込む（ファイル名: input.jpg）
     img = cv2.imread('input.jpg')
@@ -16,13 +17,18 @@ def main():
     # 輪郭抽出（Canny法という有名なアルゴリズム）
     edges = cv2.Canny(gray, 100, 200)
 
-    # エッジのみを赤で表示し、それ以外は黒にする
+    # エッジのみを青で表示し、それ以外は黒にする
     colored = np.zeros_like(img)
-    colored[edges != 0] = (0, 0, 255)
+    colored[edges != 0] = (255, 0, 0)
 
+    calsorized = cv2.addWeighted(img, 0.7, colored, 0.3, 0)
+    saiotime = cv2.addWeighted(img, 0.3, colored, 0.7, 0)
     # 結果を保存
     cv2.imwrite('output.jpg', colored)
-    print("変換完了！ output.jpg を作成しました。")
+    cv2.imwrite('calsorized.jpg', calsorized)
+    cv2.imwrite('saiotime.jpg', saiotime)
+    print("変換完了！ output.jpg, calsorized.jpg, saiotime.jpg を作成しました。")
+
 
 if __name__ == "__main__":
     main()
